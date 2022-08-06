b76a5d496bc6825df81a6f37c660f2ff81da9b12
raohan <1033234190@qq.com>
2022-08-06 12:05:33 +0800

    no message

--- src/main/java/TestOne.java
+++ src/main/java/TestOne.java
@@ -1,9 +1,9 @@
 public class TestOne {
 
-
     public static void main(String[] args) {
-        System.out.println("hello,world");
+        System.out.println("书名\t\t作者\t\t价格\t\t销量\n三国\t\t罗贯中\t120\t\t1000");
+        String name = "raohan";
+        System.out.println(name);
     }
-
 
 }
